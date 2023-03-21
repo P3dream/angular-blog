810abfd1eaee8c281ab15e2c1e84ac04570f413b
P3dream <Pedropizzi23@Hotmail.com>
2023-03-20 23:57:15 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Novo Homem de Ferro em 3D",
+    "description":"Suspendisse viverra euismod nibh nec pellentesque. Mauris nec tortor at felis aliquet mollis. Curabitur augue lorem, lacinia sit amet accumsan quis, congue ac ex. Duis malesuada pulvinar mauris sit amet pellentesque. Maecenas pellentesque tincidunt elit, sit amet finibus lacus dictum et. Maecenas efficitur sapien nec euismod imperdiet. Nulla sed ligula non massa sollicitudin venenatis. Aenean hendrerit pulvinar felis, nec varius mi gravida nec. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque interdum luctus felis, eget dictum elit consectetur eu. Nam in placerat ante. In malesuada hendrerit vehicula. Morbi pharetra quis sem ac fringilla. Maecenas mollis metus vitae leo tempus, a scelerisque arcu dignissim.",
+    "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2022/07/tony_stark_vingadores_ultimato_cena__70kqp20-1210x544.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Nova série anunciada no Disney+",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus, orci vel posuere viverra, nunc augue porttitor justo, vitae feugiat ligula tortor non velit. Quisque vitae magna condimentum tellus eleifend ullamcorper in sed quam. Fusce vitae molestie risus, nec faucibus dolor. Praesent non nulla justo. Etiam vitae nibh sit amet eros dapibus dapibus. Nulla euismod leo sit amet dui sodales, vitae egestas odio molestie. Curabitur vitae elit tincidunt, fermentum nulla sit amet, imperdiet lectus.",
+    "photoCover": "https://streamingsbrasil.com/wp-content/uploads/2020/11/series-marvel-disney-plus-.png"
+  },
+  {
+    "id":"3",
+    "title":"Novo filme do pantera negra será lançado em breve",
+    "description":"Praesent eleifend justo justo, at fringilla felis blandit vitae. Donec urna justo, elementum sed aliquet non, scelerisque quis purus. Nam tincidunt purus laoreet, congue enim in, lobortis dolor. Nam consequat eget eros sed maximus. Proin ut ornare ipsum, non semper ligula. Sed sed ante libero. Nam mattis eget libero id pretium. Etiam tempor velit id enim rhoncus, in pharetra dolor commodo. Nulla ornare finibus magna eu posuere. Integer eleifend feugiat posuere.",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6482D58C8BB4D899FA4682BF27652C3E1BB73F5971E5F963994112F26A73690/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"4",
+    "title":"Novo filme dos x-men está em pós produção, afirma diretor",
+    "description":"Donec cursus facilisis condimentum. Nunc nec sem id odio porta dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam volutpat nisi ac lacus placerat, id vehicula quam fermentum. Suspendisse pretium dui mauris, non blandit purus congue id. Aenean id sapien ut libero mollis posuere. Maecenas et pretium ex. Curabitur mollis euismod diam, ut tincidunt leo.",
+    "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  }
+]
